bbc92a18ced04b1a2aab6c9acb2ac47bfae1bcc8
wangshuxiaozu <37181168+wangshuxiaozu@users.noreply.github.com>
2018-03-09 13:29:35 +0800

    Add files via upload

---
+++ DogGame.java
@@ -0,0 +1,17 @@
+class Dog{
+	void play(){
+		System.out.println("Turn around!");
+	}
+	void bark(int number){
+		int i=0;
+		for(i=0;i<number;i++){
+			System.out.print("汪！")
+		}
+		System.out.println()
+	}
+}
+class DogGame{
+	public static void main(String[] args){
+		System.out.println("I'm role");
+	}
+} 
